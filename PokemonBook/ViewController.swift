8ab92e46c2fa5359ba5286c12576968d090928c1
Choi <chltjdw@naver.com>
2024-12-09 20:25:39 +0900

    Initial Commit

---
+++ PokemonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonBook
+//
+//  Created by t2023-m0072 on 12/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
